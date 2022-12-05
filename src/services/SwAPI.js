ea24ddb77f2e2795a2eca4dd3b3369228a6daaa9
Angela Samways <angelasamways@gmail.com>
2022-12-05 13:55:18 -0300

    req 1 - na api

--- src/services/SwAPI.js
+++ src/services/SwAPI.js
@@ -1,14 +1,14 @@
-import mock from '../mock/testData';
+// import mock from '../mock/testData';
 
-// const URL = 'https://swapi.dev/api/planets';
+const URL = 'https://swapi.dev/api/planets';
 
 const SwAPI = async () => {
-  // const request = await fetch(URL);
-  // const response = await mockFetch.json();
-  // const dado = response.results;
-  // const withoutResidents = dado.filter((item) => delete item.residents);
-  console.log(mock.results);
-  return mock.results;
+  const request = await fetch(URL);
+  const response = await request.json();
+  const dado = response.results;
+  const withoutResidents = dado.filter((item) => delete item.residents);
+  // console.log(mock.results);
+  return withoutResidents;
 };
 
 export default SwAPI;
